550c6340419a2f9030552697a3ce443dcfd6b4c9
Galih Tri Risky Andiko <galihtririskyandiko@gmail.com>
2023-03-30 13:50:22 +0700

    menambahkan link github di documentation sidebar

--- resources/views/partials/sidebar.blade.php
+++ resources/views/partials/sidebar.blade.php
@@ -59,7 +59,7 @@
             </li> --}}
 
             <div class="mt-4 mb-4 p-3 hide-sidebar-mini">
-                <a href="https://getstisla.com/docs" class="btn btn-primary btn-lg btn-block btn-icon-split">
+                <a href="https://github.com/galihtra/koperasi_polibatam" class="btn btn-primary btn-lg btn-block btn-icon-split">
                     <i class="fas fa-rocket"></i> Documentation
                 </a>
             </div>
